b56534c4a673b11591b9042b13b096c769e63735
quamrulmina <quamrul@pragmasys.com>
2015-10-25 17:33:46 -0500

    Microsoft Copyright message added to all new files
    New files for openssh win32 port now contain MS Copyright texts.

--- contrib/win32/win32compat/cng_digest.c
+++ contrib/win32/win32compat/cng_digest.c
@@ -1,20 +1,37 @@
-/* digest_cng.c
-* Author: Pragma Systems, Inc. <www.pragmasys.com>
-* Contribution by Pragma Systems, Inc. for Microsoft openssh win32 port
-* Copyright (c) 2011, 2015 Pragma Systems, Inc.
-* All rights reserved
-*
-* Common library for Windows Console Screen IO.
-* Contains Windows console related definition so that emulation code can draw
-* on Windows console screen surface.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice.
-* 2. Binaries produced provide no direct or implied warranties or any
-*    guarantee of performance or suitability.
-*/
+/*
+ * Author: Microsoft Corp.
+ *
+ * Copyright (c) 2015 Microsoft Corp.
+ * All rights reserved
+ *
+ * Microsoft openssh win32 port
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/* cng_digest.c
+ *
+ * Openssh digests implemented using Microsoft Crypto Next Generation (CNG).
+ *
+ */
 
 typedef unsigned int u_int;
 typedef unsigned char u_char;
