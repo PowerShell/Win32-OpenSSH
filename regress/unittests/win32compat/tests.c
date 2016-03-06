d8a231f2ea37db8dc43682331f7d5cb8e10a8f80
manojampalam <manojampalam@live.com>
2016-03-05 16:17:24 -0800

    3-5 c5

--- regress/unittests/win32compat/tests.c
+++ regress/unittests/win32compat/tests.c
@@ -2,7 +2,13 @@
 * Author: Manoj Ampalam <manoj.ampalam@microsoft.com>
 */
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <io.h>
 #include "test_helper.h"
+
+extern void log_init(char *av0, int level, int facility, int on_stderr);
+extern  int logfd;
 
 void socket_tests();
 void file_tests();
@@ -10,8 +16,10 @@ void file_tests();
 void tests(void)
 {
     _set_abort_behavior(0, 1);
-
+    log_init(NULL, 7, 2, 0);
+    logfd = _open("unittests.log", O_WRONLY | O_CREAT );
     socket_tests();
     file_tests();
+    _close(logfd);
     return;
 }
