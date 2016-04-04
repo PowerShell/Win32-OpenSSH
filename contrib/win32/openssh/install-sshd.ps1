473539b9bf9db7641d14a5a97138b3c6b3d1820b
manojampalam <manojampalam@live.com>
2016-04-03 21:06:52 -0700

    install and unintall scripts for sshd

---
+++ contrib/win32/openssh/install-sshd.ps1
@@ -0,0 +1,17 @@
+﻿$scriptpath = $MyInvocation.MyCommand.Path
+$scriptdir = Split-Path $scriptpath
+
+$sshdpath = Join-Path $scriptdir "sshd.exe"
+
+if (-not (Test-Path $sshdpath)) {
+    throw "sshd.exe is not present in script path"
+}
+
+if (Get-Service sshd -ErrorAction SilentlyContinue) 
+{
+   Stop-Service sshd
+   sc.exe delete sshd 1> null
+}
+
+New-Service -Name sshd -BinaryPathName $sshdpath -Description "SSH Deamon" -StartupType Manual | Out-Null
+Write-Host -ForegroundColor Green "sshd successfully installed"
---
+++ contrib/win32/openssh/uninstall-sshd.ps1
@@ -0,0 +1,10 @@
+﻿if (Get-Service sshd -ErrorAction SilentlyContinue) 
+{
+   Stop-Service sshd
+   sc.exe delete sshd 1> null
+   Write-Host -ForegroundColor Green "sshd successfully uninstalled"
+}
+else {
+    Write-Host -ForegroundColor Yellow "sshd service is not installed"
+}
+
