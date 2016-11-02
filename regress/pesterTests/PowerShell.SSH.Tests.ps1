12dbd960979b7c3251139bfe397a7788037a81cb
bingbing8 <yawang@microsoft.com>
2016-11-01 18:59:20 -0700

    Initial tests check in for SCP, SSH, powershell over ssh, and portforwarding
    Initial check in for SCP, SSH, powershell over ssh, and portforwarding

---
+++ regress/pesterTests/PowerShell.SSH.Tests.ps1
@@ -0,0 +1,74 @@
+﻿using module .\PlatformAbstractLayer.psm1
+
+Describe "Tests for powershell over ssh" -Tags "CI" {
+    BeforeAll {
+        
+        [Machine] $client = [Machine]::new([MachineRole]::Client)
+        [Machine] $server = [Machine]::new([MachineRole]::Server)
+        $client.SetupClient($server)
+        $server.SetupServer($client)
+        $server.SetupServerRemoting([Protocol]::SSH)
+    }
+    AfterAll {
+        $client.CleanupClient()
+        $server.CleanupServer()
+    }
+
+    Context "Key based authentication with KeyFilePath. Key is Secured in ssh-agenton server" {
+        BeforeAll {
+            $server.SecureHostKeys($server.PrivateHostKeyPaths)
+            $identifyFile = $client.clientPrivateKeyPaths[0]
+        }
+
+        AfterAll {
+            $server.CleanupHostKeys()
+        }        
+        It 'Key is Secured in ssh-agenton server' {
+            $session = New-PSSession -HostName $server.MachineName -UserName $server.localAdminUserName -KeyFilePath $identifyFile
+            #$pscreds = [System.Management.Automation.PSCredential]::new($($server.MachineName) + "\" + $($server.localAdminUserName), $($server.password))
+            #$session = New-PSSession -Credential $pscreds -ComputerName $($server.MachineName)
+            $ret = Invoke-Command $session -command {$env:computername}
+            $ret | Should be $server.MachineName
+        }
+    }
+
+    #this context only run on windows
+    Context "Single signon and host keys are secured in ssh-agent" {
+        BeforeAll {        
+            $server.SecureHostKeys($server.PrivateHostKeyPaths)
+            $identifyFile = $client.clientPrivateKeyPaths[0]
+            #setup single signon
+            .\ssh-add.exe $identifyFile
+        }
+
+        AfterAll {
+            $server.CleanupHostKeys()
+
+            #cleanup single signon
+            .\ssh-add.exe -D
+        }
+        
+        It 'Single signon and host keys are secured in ssh-agent' {
+            #$session = New-PSSession -HostName $server.MachineName -UserName $server.localAdminUserName
+            $pscreds = [System.Management.Automation.PSCredential]::new($($server.MachineName) + "\" + $($server.localAdminUserName), $($server.password))
+            $session = New-PSSession -Credential $pscreds -ComputerName $($server.MachineName)
+            $ret = Invoke-Command $session -command {$env:computername}
+            $ret | Should be $server.MachineName
+        }
+    }    
+   
+   Context "Key based authentication with KeyFilePath. Host keys are not secured on server" {
+        BeforeAll {
+            $identifyFile = $client.clientPrivateKeyPaths[0]
+        }
+        
+        It 'Key based authentication with KeyFilePath. Host keys are not secured on server' {
+            $session = New-PSSession -HostName $server.MachineName -UserName $server.localAdminUserName -KeyFilePath $identifyFile
+            #$pscreds = [System.Management.Automation.PSCredential]::new($($server.MachineName) + "\" + $($server.localAdminUserName), $($server.password))
+            #$session = New-PSSession -Credential $pscreds -ComputerName $($server.MachineName)
+            $ret = Invoke-Command $session -command {$env:computername}
+            $ret | Should be $server.MachineName
+        }
+    }    
+}   
+
---
+++ regress/pesterTests/SSH.Tests.ps1
@@ -0,0 +1,139 @@
+﻿using module .\PlatformAbstractLayer.psm1
+
+#covered -i -q -v -l -c -C
+#todo: -S -F -V -e
+Describe "Tests for ssh command" -Tags "CI" {
+    BeforeAll {        
+        $fileName = "test.txt"
+        $filePath = Join-Path ${TestDrive} $fileName
+
+        [Machine] $client = [Machine]::new([MachineRole]::Client)
+        [Machine] $server = [Machine]::new([MachineRole]::Server)
+        $client.SetupClient($server)
+        $server.SetupServer($client)
+
+        $testData = @(
+            @{
+                Title = 'Simple logon -v option';                
+                LogonStr = "$($server.localAdminUserName)@$($server.MachineName)"
+                Options = "-v"
+            },
+            @{
+                Title = 'Simple logon using -C -l option'
+                LogonStr = $server.MachineName
+                Options = "-C -l $($server.localAdminUserName)"
+            }
+        )
+        
+        $testData1 = @(
+            @{
+                Title = "logon using -i -q option"
+                LogonStr = "$($server.localAdminUserName)@$($server.MachineName)"
+                Options = '-i $identifyFile -q'
+            },
+            @{
+                Title = "logon using -i -v option"
+                LogonStr = "$($server.localAdminUserName)@$($server.MachineName)"
+                Options = '-i $identifyFile -v'
+            },
+            @{
+                Title = "logon using -i -c  option"
+                LogonStr = "$($server.localAdminUserName)@$($server.MachineName)"
+                Options = '-i $identifyFile -c aes256-ctr'
+            },
+            <# -V does not redirect to file
+            @{
+                Title = "logon using -i -V option"
+                LogonStr = "$($server.localAdminUserName)@$($server.MachineName)"
+                Options = '-i $identifyFile -V'
+                SkipVerification = $true
+            },#>
+            @{
+                Title = 'logon using -i -l option'
+                LogonStr = $server.MachineName
+                Options = '-i $identifyFile -l $($server.localAdminUserName)'
+            }
+        )
+        
+    }
+
+    AfterAll {
+        $client.CleanupClient()
+        $server.CleanupServer()
+    }
+
+    Context "Key is not secured in ssh-agent on server" {
+        BeforeAll {            
+            $identifyFile = $client.clientPrivateKeyPaths[0]
+            Remove-Item -Path $filePath -Force -ea silentlycontinue
+        }
+        
+        AfterEach {
+            Remove-Item -Path $filePath -Force -ea silentlycontinue
+        }
+        
+        It '<Title>' -TestCases:$testData1 {
+            param([string]$Title, $LogonStr, $Options, $SkipVerification = $false)
+           
+           $str = $ExecutionContext.InvokeCommand.ExpandString(".\ssh $($Options) $($LogonStr) hostname > $filePath")
+           $client.RunCmd($str)
+           #validate file content.
+           Get-Content $filePath | Should be $server.MachineName           
+        }
+    }
+    
+    Context "Key is secured in ssh-agent" {
+        BeforeAll {
+            $server.SecureHostKeys($server.PrivateHostKeyPaths)
+            $identifyFile = $client.clientPrivateKeyPaths[0]
+            Remove-Item -Path $filePath -Force -ea silentlycontinue
+        }
+
+        AfterAll {            
+            $Server.CleanupHostKeys()
+        }
+        
+        AfterEach {
+            Remove-Item -Path $filePath -Force -ea silentlycontinue
+        }
+        
+        It '<Title>' -TestCases:$testData1 {
+            param([string]$Title, $LogonStr, $Options, $SkipVerification = $false)
+           
+           $str = $ExecutionContext.InvokeCommand.ExpandString(".\ssh $Options $LogonStr hostname > $filePath")
+           $client.RunCmd($str)
+           #validate file content.           
+           Get-Content $filePath | Should be $server.MachineName           
+        }
+    }
+    
+    Context "Single signon on client and keys secured in ssh-agent on server" {
+        BeforeAll {
+            $Server.SecureHostKeys($server.PrivateHostKeyPaths)
+            $identifyFile = $client.clientPrivateKeyPaths[0]
+            #setup single signon
+            .\ssh-add.exe $identifyFile
+            Remove-Item -Path $filePath -Force -ea silentlycontinue
+        }
+
+        AfterAll {
+            $Server.CleanupHostKeys()
+
+            #cleanup single signon
+            .\ssh-add.exe -D
+        }
+        
+        AfterEach {
+            Remove-Item -Path $filePath -Force -ea silentlycontinue
+        }
+
+        It '<Title>' -TestCases:$testData {
+            param([string]$Title, $LogonStr, $Options)
+           
+           $str = ".\ssh $($Options) $($LogonStr) hostname > $filePath"
+           $client.RunCmd($str)
+           #validate file content.           
+           Get-Content $filePath | Should be $server.MachineName           
+        }
+    }    
+}
