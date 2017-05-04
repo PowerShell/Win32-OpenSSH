6b6b99d3d6da39101386c3d62ffe3da0f15d112d
bagajjal <bagajjal@microsoft.com>
2017-05-03 17:28:21 -0700

    Adding libressl sdk

---
+++ contrib/win32/openssh/LibreSSLSDK/include/openssl/opensslv.h
@@ -0,0 +1,17 @@
+/* $OpenBSD: opensslv.h,v 1.39.4.1 2017/04/06 10:36:12 bcook Exp $ */
+#ifndef HEADER_OPENSSLV_H
+#define HEADER_OPENSSLV_H
+
+/* These will change with each release of LibreSSL-portable */
+#define LIBRESSL_VERSION_NUMBER	0x2050300fL
+#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.5.3"
+
+/* These will never change */
+#define OPENSSL_VERSION_NUMBER	0x20000000L
+#define OPENSSL_VERSION_TEXT	LIBRESSL_VERSION_TEXT
+#define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
+
+#define SHLIB_VERSION_HISTORY ""
+#define SHLIB_VERSION_NUMBER "1.0.0"
+
+#endif /* HEADER_OPENSSLV_H */
